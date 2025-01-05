586a44e1056246c7254a5cbf58d513383343b777
Sind96 <sindhujany@gmail.com>
2025-01-05 20:54:31 +0000

    Controller updated.

--- server/controller/todo.js
+++ server/controller/todo.js
@@ -37,6 +37,10 @@ exports.putTasks = async (req, res) => {
 
 exports.deleteTasks = async (req, res) => {
   try {
+    const { id } = req.params;
+    const deletedTask = await toDoList.findByIdAndDelete(id);
+    if (!deletedTask) return res.status(404).send("Task not found");
+    res.status(200).send("Task deleted successfully");
   } catch (error) {
     console.log(`There has been an error with deleteTasks:`, error);
     res.status(500).send(`Internal Server Error`);
@@ -45,6 +49,8 @@ exports.deleteTasks = async (req, res) => {
 
 exports.getTasks = async (req, res) => {
   try {
+    const tasks = await toDoList.find();
+    res.status(200).send(tasks);
   } catch (error) {
     console.log(`There has been an error with getTasks:`, error);
     res.status(500).send(`Internal Server Error`);

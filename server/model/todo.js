f5e3a5802d6304049feb53acf30547d2c973ace8
Sind96 <sindhujany@gmail.com>
2025-01-05 22:21:25 +0000

    Controller functions working successfully in Postman

--- server/model/todo.js
+++ server/model/todo.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const toDoSchema = new Schema({
+const toDoListSchema = new Schema({
   title: String,
   completed: Boolean,
   createdAt: Date,
   updatedAt: Date,
 });
 
-const toDoList = mongoose.model("toDoList", toDoSchema);
+const toDoList = mongoose.model("toDoList", toDoListSchema);
+
+module.exports = toDoList;

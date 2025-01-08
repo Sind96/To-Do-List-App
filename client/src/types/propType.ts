60dc7c6e54434a6ea7c6098a79be582ea5ea5446
Sind96 <sindhujany@gmail.com>
2025-01-08 14:50:44 +0000

    TaskProps added to props.

---
+++ client/src/types/propType.ts
@@ -0,0 +1,7 @@
+export interface TaskProps {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
--- client/src/types/tasks.ts
+++
@@ -1,5 +0,0 @@
-export interface TaskItemProps {
-  id: string;
-  title: string;
-  completed: boolean;
-}

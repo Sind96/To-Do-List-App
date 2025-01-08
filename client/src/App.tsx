b823b6d5d6983cb75980f61064b93ef447df8bb7
Sind96 <sindhujany@gmail.com>
2025-01-08 22:45:39 +0000

    App.ts - added loadTasks using useEffect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,29 @@
 import "./index.css";
+import React, { useEffect, useState } from "react";
+import {
+  fetchTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+} from "./services/api.services";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import { TaskProps } from "./types/propType";
 
 function App() {
+  const [tasks, setTasks] = useState<TaskProps[]>([]);
+  const [filter, setFilter] = useState<"All" | "Completed" | "Incomplete">(
+    "All"
+  );
+
+  useEffect(() => {
+    const loadTasks = async () => {
+      const fetchedTasks = await fetchTasks();
+      setTasks(fetchedTasks);
+    };
+    loadTasks();
+  }, []);
+
   return (
     <>
       <p className="text-blue-500">Hello</p>

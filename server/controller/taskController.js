3708690377e2c7ba14eb9b90a2bcaab33473f1b9
Sind96 <sindhujany@gmail.com>
2025-02-13 18:39:36 +0000

    taskController: changing .send to .json.

--- server/controller/taskController.js
+++ server/controller/taskController.js
@@ -7,6 +7,7 @@ export const getTasks = async (req, res) => {
       ...task,
       id: task._id.toString(),
     }));
+
     res.status(200).json({
       message: "Tasks fetched successfully",
       tasks: formattedTasks,
@@ -20,7 +21,7 @@ export const getTasks = async (req, res) => {
 export const postTask = async (req, res) => {
   try {
     const { title } = req.body;
-    if (!title) return res.status(400).send({ error: "Title is required" });
+    if (!title) return res.status(400).json({ error: "Title is required" });
 
     const newTask = new Task({
       title,
@@ -30,7 +31,7 @@ export const postTask = async (req, res) => {
     });
 
     await newTask.save();
-    res.status(201).send(newTask);
+    res.status(201).json(newTask);
   } catch (error) {
     console.error("Error in postTask:", error);
     res.status(500).json({ message: "Internal Server error" });
@@ -48,9 +49,9 @@ export const putTask = async (req, res) => {
       { new: true }
     );
 
-    if (!updatedTask) return res.status(404).send({ error: "Task not found" });
+    if (!updatedTask) return res.status(404).json({ error: "Task not found" });
 
-    res.status(200).send(updatedTask);
+    res.status(200).json(updatedTask);
   } catch (error) {
     console.error("Error in putTask:", error);
     res.status(500).json({ message: "Internal Server error" });
@@ -63,9 +64,9 @@ export const deleteTask = async (req, res) => {
 
     const deletedTask = await Task.findByIdAndDelete(id);
 
-    if (!deletedTask) return res.status(404).send({ error: "Task not found" });
+    if (!deletedTask) return res.status(404).json({ error: "Task not found" });
 
-    res.status(200).send({ message: "Task deleted successfully" });
+    res.status(200).json({ message: "Task deleted successfully" });
   } catch (error) {
     console.error("Error in deleteTask:", error);
     res.status(500).json({ message: "Internal Server error" });

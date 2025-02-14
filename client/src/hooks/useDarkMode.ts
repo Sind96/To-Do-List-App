ea04baa21e2bcbb31bdaeafa5cd3310ab751a0ba
Sind96 <sindhujany@gmail.com>
2025-02-14 00:43:35 +0000

    useDarkMode: Added useDarkMode hook.

---
+++ client/src/hooks/useDarkMode.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export default function useDarkMode() {
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    return localStorage.getItem("theme") === "dark";
+  });
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [isDarkMode]);
+
+  return [isDarkMode, setIsDarkMode] as const;
+}

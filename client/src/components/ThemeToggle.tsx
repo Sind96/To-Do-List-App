564bbc960dd64b5132d260cee764766e7dab3da4
Sind96 <sindhujany@gmail.com>
2025-02-14 12:45:39 +0000

    ThemeToggle: Adding toggle buttons.

---
+++ client/src/components/ThemeToggle.tsx
@@ -0,0 +1,17 @@
+import useDarkMode from "../hooks/useDarkMode";
+import { ToggleLeft, ToggleRight } from "lucide-react";
+
+const ThemeToggle = () => {
+  const [darkMode, setDarkMode] = useDarkMode();
+
+  return (
+    <button
+      onClick={() => setDarkMode(!darkMode)}
+      className="p-2 rounded-full bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-white transition-all"
+    >
+      {darkMode ? <ToggleLeft size={24} /> : <ToggleRight size={24} />}
+    </button>
+  );
+};
+
+export default ThemeToggle;

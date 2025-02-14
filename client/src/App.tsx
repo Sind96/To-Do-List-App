66289297de193c7ad0176f110506183a41cd900a
Sind96 <sindhujany@gmail.com>
2025-02-14 13:27:53 +0000

    Final Commit.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ import ThemeToggle from "./components/ThemeToggle.js";
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <div className="max-w-2xl mx-auto p-6 min-h-screen bg-gray-100 dark:bg-gray-800 relative transition-colors duration-300">
+      <div className="max-w-2xl mx-auto p-4 rounded-t-md min-h-screen bg-gray-100 dark:bg-gray-800 relative transition-colors duration-300">
         <div className="absolute top-10 right-10">
           <ThemeToggle />
         </div>

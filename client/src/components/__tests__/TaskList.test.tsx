dd065c427ab96ce06da8dd775e96a316e260ef71
Sind96 <sindhujany@gmail.com>
2025-04-16 22:36:44 +0100

    TaskList.test: Adding mockTasks.

--- client/src/components/__tests__/TaskList.test.tsx
+++ client/src/components/__tests__/TaskList.test.tsx
@@ -1,3 +1,22 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import TaskList from "../TaskList.tsx";
 import { TaskProps } from "../@types/task.types.ts";
+
+describe("", () => {
+  const mockTasks: TaskProps[] = [
+    {
+      _id: "1",
+      title: "Test Task 1",
+      completed: false,
+      createdAt: "",
+      updatedAt: "",
+    },
+    {
+      _id: "2",
+      title: "Test Task 2",
+      completed: true,
+      createdAt: "",
+      updatedAt: "",
+    },
+  ];
+});

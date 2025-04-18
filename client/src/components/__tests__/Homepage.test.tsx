dae227323be3fd44f40b8b9cb6a846b7143d25d9
Sind96 <sindhujany@gmail.com>
2025-04-18 19:33:57 +0100

    Homepage.test: Created mockData and mocked the service function.

--- client/src/components/__tests__/Homepage.test.tsx
+++ client/src/components/__tests__/Homepage.test.tsx
@@ -2,3 +2,14 @@ import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import HomePage from "../../pages/HomePage.js";
 import * as api from "../../services/api.services.ts";
 
+const mockTasks = [
+  { _id: "1", title: "Task One", completed: false },
+  { _id: "2", title: "Task Two", completed: true },
+];
+
+jest.mock("../../services/api.services.ts")
+
+const mockFetch = api.fetchTasks as jest.Mock;
+const mockCreate = api.createTask as jest.Mock;
+const mockUpdate = api.updateTask as jest.Mock;
+const mockDelete = api.deleteTask as jest.Mock;

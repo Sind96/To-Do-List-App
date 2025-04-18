f00785b01f1625279b4f34888d71c7219aab4fc2
Sind96 <sindhujany@gmail.com>
2025-04-18 13:42:50 +0100

    Removed old testing.

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -1,9 +1,1 @@
 import "@testing-library/jest-dom";
-import { server } from "./mocks/server";
-
-// Start API mocking before all tests
-beforeAll(() => server.listen());
-// Reset any request handlers that are declared during the tests
-afterEach(() => server.resetHandlers());
-// Clean up after all tests
-afterAll(() => server.close());
